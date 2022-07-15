ecac179e9d95dbef06b4853e9eda58a9db57787a
Steve Kieffer <skieffer@alpinemath.org>
2022-07-15 09:10:50 -0400

    Version 0.22.7

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    long_description = ''.join(fh.readlines()[1:])
 
 setuptools.setup(
     name="pfsc-examp",
-    version="0.22.7-pre",
+    version="0.22.7",
     license="Apache 2.0",
     url="https://github.com/alpinemath/pfsc-examp",
     description="Example explorers for Proofscape",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'lark067==0.6.7',
+        'alpinemath-sympy>=0.10.3',
+        'displaylang>=0.22.7',
+        'pfsc-util>=0.22.7',
+        'Jinja2>=3.0.3,<4',
+        'MarkupSafe>=2.0.1,<3',
+    ],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
